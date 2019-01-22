a6a91a8dd32ceddad8ad7f111c2a8878df4071d6
JimvandeVen <jimvandeven_1@hotmail.com>
2019-01-22 13:53:44 +0100

    added style to the social icons

--- javascript.js
+++ javascript.js
@@ -87,12 +87,4 @@ function checkSlide(e){
   })
 }
 
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/nl_NL/sdk.js#xfbml=1&version=v3.2';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
 window.addEventListener("scroll", debounce(checkSlide, 100));
